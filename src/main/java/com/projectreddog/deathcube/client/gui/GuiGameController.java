bc55ce31132f168e08497690058821415d194a21
Trayer <trayer3@gmail.com>
2015-04-07 17:34:51 -0400

    Network Mess
    Tried to copy MineMaarten.  Not even close to working yet.

--- src/main/java/com/projectreddog/deathcube/client/gui/GuiGameController.java
+++ src/main/java/com/projectreddog/deathcube/client/gui/GuiGameController.java
@@ -1,8 +1,11 @@
 package com.projectreddog.deathcube.client.gui;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 
+import com.projectreddog.deathcube.network.MessageHandleGuiButtonPress;
+import com.projectreddog.deathcube.network.NetworkHandler;
 import com.projectreddog.deathcube.reference.Reference;
 import com.projectreddog.deathcube.tileentity.TileEntityGameController;
 
@@ -10,6 +13,7 @@ public class GuiGameController extends GuiScreen {
 
 	private TileEntityGameController game_controller;
 	private GuiTextField textField;
+	private GuiButton startButton;
 	private int gui_Width = 225;
 	private int gui_Height = 137;
 	private int x = 0;
@@ -23,13 +27,22 @@ public class GuiGameController extends GuiScreen {
 	@Override
 	public void initGui() {
 		super.initGui();
-		// resetButton = new GuiButton(0, guiLeft + 10, guiTop + 37, 40, 20, "");
-		// buttonList.add(resetButton);
 
-		// Set up text fields
+		/**
+		 * Find GUI upper-left corner.
+		 */
 		x = (this.width - this.gui_Width) / 2;
 		y = (this.height - this.gui_Height) / 2;
+		
+		/**
+		 * Prepare Button
+		 */
+		startButton = new GuiButton(0, x + 10, y + 37, 40, 20, "");
+		buttonList.add(startButton);
 
+		/**
+		 * Prepare Text Field
+		 */
 		textField = new GuiTextField(20, fontRendererObj, x + 60, y + 20, 70, 12);
 		textField.setMaxStringLength(40);
 		textField.setText("Red");
@@ -64,6 +77,17 @@ public class GuiGameController extends GuiScreen {
 	public boolean doesGuiPauseGame() {
 		return false;
 	}
+	
+	@Override
+    protected void actionPerformed(GuiButton button){
+        if(button.id == 0) NetworkHandler.sendToServer(new MessageHandleGuiButtonPress(te, 0));
+    }
+	
+	@Override
+    public void updateScreen(){
+        super.updateScreen();
+        startButton.displayString = "Start Game!";
+    }
 
 	@Override
 	public void onGuiClosed() {
--- src/main/java/com/projectreddog/deathcube/proxy/IProxy.java
+++ src/main/java/com/projectreddog/deathcube/proxy/IProxy.java
@@ -1,7 +1,11 @@
 package com.projectreddog.deathcube.proxy;
 
+import net.minecraft.entity.player.EntityPlayer;
+
 public interface IProxy {
 	
 	void registerRenderers();
+
+	EntityPlayer getClientPlayer();
 	
 }
