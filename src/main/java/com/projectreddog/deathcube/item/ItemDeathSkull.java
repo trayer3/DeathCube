bbf5346d0501817f4ccd5e7518c327dcd462c324
TechStack <techStackLp@gmail.com>
2015-06-04 20:09:09 -0400

    adding several debug statements can be reverted

--- src/main/java/com/projectreddog/deathcube/item/ItemDeathSkull.java
+++ src/main/java/com/projectreddog/deathcube/item/ItemDeathSkull.java
@@ -11,6 +11,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 import com.projectreddog.deathcube.reference.Reference;
+import com.projectreddog.deathcube.utility.Log;
 
 public class ItemDeathSkull extends ItemDeathCube {
 
@@ -20,113 +21,128 @@ public class ItemDeathSkull extends ItemDeathCube {
 		this.maxStackSize = 1;
 		this.setMaxDamage(Reference.ITEM_DEATHSKULL_DURABILITY);
 	}
-	
+
 	/**
-     * Called when the player Left Clicks (attacks) an entity.
-     * Processed before damage is done, if return value is true further processing is canceled
-     * and the entity is not attacked.
-     *
-     * @param stack The Item being used
-     * @param player The player that is attacking
-     * @param entity The entity being attacked
-     * @return True to cancel the rest of the interaction.
-     */
+	 * Called when the player Left Clicks (attacks) an entity.
+	 * Processed before damage is done, if return value is true further processing is canceled
+	 * and the entity is not attacked.
+	 *
+	 * @param stack
+	 *            The Item being used
+	 * @param player
+	 *            The player that is attacking
+	 * @param entity
+	 *            The entity being attacked
+	 * @return True to cancel the rest of the interaction.
+	 */
 	@Override
-	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
-    {
-		//if(!player.worldObj.isRemote) {
-			Random rand = new Random();
-			float randAmountx = rand.nextFloat()/2;
-			float randAmountz = rand.nextFloat()/2;
-			if(rand.nextBoolean()) {
-				randAmountx = -randAmountx;
-			}
-			if(rand.nextBoolean()) {
-				randAmountz = -randAmountz;
-			}
-			
-			if(entity instanceof EntityPlayer) {
-				stack.damageItem(1, player);
-				((EntityPlayer) entity).addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
-			} else if(entity instanceof EntityPlayerMP) {
-				stack.damageItem(1, player);
-				((EntityPlayerMP) entity).addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
-			} else {
-				stack.damageItem(1, player);
-				entity.addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
-			}
-		//}
-		
-        return true;
-    }
-	
-	/**
-     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
-     */
-	@Override
-    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
-    {
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+		// if(!player.worldObj.isRemote) {
 		Random rand = new Random();
-		float randAmountx = rand.nextFloat()/5;
-		float randAmountz = rand.nextFloat()/5;
-		if(rand.nextBoolean()) {
+		float randAmountx = rand.nextFloat() / 2;
+		float randAmountz = rand.nextFloat() / 2;
+		Log.info("x , z randmount:" + randAmountx + " " + randAmountz);
+		if (rand.nextBoolean()) {
 			randAmountx = -randAmountx;
 		}
-		if(rand.nextBoolean()) {
+		if (rand.nextBoolean()) {
 			randAmountz = -randAmountz;
 		}
-		
-		if(playerIn.onGround) {
+		Log.info("x , z randmount2:" + randAmountx + " " + randAmountz);
+
+		if (entity instanceof EntityPlayer) {
+			stack.damageItem(1, player);
+			((EntityPlayer) entity).addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
+			Log.info("in entityPlayer");
+
+		} else if (entity instanceof EntityPlayerMP) {
+			stack.damageItem(1, player);
+			((EntityPlayerMP) entity).addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
+			Log.info("in entityPlayer");
+		} else {
+			stack.damageItem(1, player);
+			entity.addVelocity(randAmountx, (Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT * 2), randAmountz);
+			Log.info("in Else");
+		}
+		// }
+
+		return true;
+	}
+
+	/**
+	 * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
+	 */
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
+		Random rand = new Random();
+		float randAmountx = rand.nextFloat() / 5;
+		float randAmountz = rand.nextFloat() / 5;
+		if (rand.nextBoolean()) {
+			randAmountx = -randAmountx;
+		}
+		if (rand.nextBoolean()) {
+			randAmountz = -randAmountz;
+		}
+
+		if (playerIn.onGround) {
 			itemStackIn.damageItem(1, playerIn);
 			playerIn.addVelocity(randAmountx, Reference.ITEM_DEATHSKULL_VELOCITY_AMOUNT, randAmountz);
 		}
-		
-        return itemStackIn;
-    }
-    
-    /**
-     * This is called when the item is used, before the block is activated.
-     * @param stack The Item Stack
-     * @param player The Player that used the item
-     * @param world The Current World
-     * @param pos Target position
-     * @param side The side of the target hit
-     * @return Return true to prevent any further processing.
-     */
-	@Override
-    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
-    {
-        return false;
-    }
 
-    /**
-     * Called each tick while using an item.
-     * @param stack The Item being used
-     * @param player The Player using the item
-     * @param count The amount of time in tick the item has been used for continuously
-     */
-	@Override
-    public void onUsingTick(ItemStack stack, EntityPlayer player, int count)
-    {
-    }
-    
-    /**
-     * Called when the player finishes using this Item (E.g. finishes eating.). Not called when the player stops using
-     * the Item before the action is complete.
-     */
-	@Override
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn)
-    {
-        return stack;
-    }
-	
+		return itemStackIn;
+	}
+
 	/**
-     * Called when the player stops using an Item (stops holding the right mouse button).
-     *  
-     * @param timeLeft The amount of ticks left before the using would have been complete
-     */
+	 * This is called when the item is used, before the block is activated.
+	 * 
+	 * @param stack
+	 *            The Item Stack
+	 * @param player
+	 *            The Player that used the item
+	 * @param world
+	 *            The Current World
+	 * @param pos
+	 *            Target position
+	 * @param side
+	 *            The side of the target hit
+	 * @return Return true to prevent any further processing.
+	 */
 	@Override
-    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityPlayer playerIn, int timeLeft) {
-		
+	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
+		return false;
+	}
+
+	/**
+	 * Called each tick while using an item.
+	 * 
+	 * @param stack
+	 *            The Item being used
+	 * @param player
+	 *            The Player using the item
+	 * @param count
+	 *            The amount of time in tick the item has been used for continuously
+	 */
+	@Override
+	public void onUsingTick(ItemStack stack, EntityPlayer player, int count) {
+	}
+
+	/**
+	 * Called when the player finishes using this Item (E.g. finishes eating.). Not called when the player stops using
+	 * the Item before the action is complete.
+	 */
+	@Override
+	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn) {
+		return stack;
+	}
+
+	/**
+	 * Called when the player stops using an Item (stops holding the right mouse button).
+	 * 
+	 * @param timeLeft
+	 *            The amount of ticks left before the using would have been complete
+	 */
+	@Override
+	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityPlayer playerIn, int timeLeft) {
+
 	}
 }
